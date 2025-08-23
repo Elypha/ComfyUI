695dfc74f801a2850e5dbcdff3bfbf80b909a6d9
Elypha Grey <i@elypha.com>
2025-08-24 00:51:17 +0900

    add launcher script

---
+++ run_backend.bat
@@ -0,0 +1,6 @@
+@REM params:
+@REM add --listen to enable listening on 0.0.0.0
+@REM add --disable-auto-launch to disable auto-launching the browser
+
+uv run main.py --listen --disable-auto-launch
+pause
